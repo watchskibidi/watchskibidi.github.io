57ca673ec1d612fa376fbff953eefc0ffa6fcb33
cobblesteve-dev <channelediting521@gmail.com>
2024-12-27 11:51:18 -0600

    Some "minor" changes

--- main.js
+++ main.js
@@ -11,5 +11,19 @@ function fullscreen() {
     movie.style.borderRadius = '0';
     movie.style.top = '0';
     movie.style.left = '0';
-    gofullscreenlscreen.style.display = 'none';
+    gofullscreen.style.display = 'none';
 }
+
+function exitFullscreen() {
+    movie.style.width = '1050px';
+    movie.style.height = '590px';
+    movie.style.borderRadius = '20px';
+    movie.style.position = 'relative';
+    gofullscreen.style.display = 'block';
+}
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+        exitFullscreen();
+    }
+});
