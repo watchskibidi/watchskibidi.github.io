0300f8de2b763c57735a53e27a463e1725f8016d
cobblesteve-dev <channelediting521@gmail.com>
2024-12-27 11:23:07 -0600

    Add fullscreen function

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 console.log('skibidi toilet')
 let skibiditoilet;
 
-function skibiditoilet() {
-    document.addEventListener()
+var movie = document.getElementById('embed');
+var gofullscreen = document.getElementById('gofullscreen');
+
+function fullscreen() {
+    movie.style.width = '100%';
+    movie.style.height = '100vh';
+    movie.style.position = 'fixed';
+    movie.style.borderRadius = '0';
+    movie.style.top = '0';
+    movie.style.left = '0';
+    gofullscreenlscreen.style.display = 'none';
 }
